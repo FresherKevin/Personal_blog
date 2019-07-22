667ce8d2c9627101504759a2d25016c98f981ceb
kevin <chaokai_xu@163.com>
2019-07-22 21:22:52 +0800

    增加star模块

---
+++ src/main/java/com/blog/service/impl/StartService.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2018-2019,
+ * FileName: StartService
+ * Author:   Kevin
+ * Date:     2019/7/22 21:18
+ * Description: 点赞服务
+ */
+package com.blog.service.impl;
+
+/**
+ * 〈点赞服务〉
+ *
+ * @author Kevin
+ * @create 2019/7/22
+ * @since 1.0.0
+ */
+public class StartService {
+
+  /**
+    star and unstar
+  **/
+
+}
